487ae26b7046d18d0b2bd13a81798f9898d60266
Lizza Reyes <lizzareyes13@gmail.com>
2017-03-31 05:46:05 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$dbhost = 'localhost/sakila-db';
+
+
+$styleInner = 'style="float: left; width: 100px; border: 1px solid black; text-align: center;"';
+
+$styleOuter = 'style="float: left; width: 700px;"';
+
+echo "<div $styleOuter>";
+
+//syntax: for ( initialization; condition; increment){//code;}
+
+for ($x = 0; $x < 6; $x++) {
+  echo "<div $styleInner>";
+  echo $movies[$x];
+  echo '</div>';
+  echo PHP_EOL;
+}
+echo '</div>';
